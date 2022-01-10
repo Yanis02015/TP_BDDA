cd22c2d02745293058b8469aea1c220e03c655b5
Yanis Oulhaci <yaniscrab@gmail.com>
2022-01-10 21:13:05 +0100

    TP 2 presque fini

--- TP1 - BDDA.sql
+++ TP1 - BDDA.sql
@@ -2,11 +2,11 @@ CREATE TYPE adresse_type AS OBJECT (
     numeroRue int,
     nomRue VARCHAR2(50),
     codePostal VARCHAR2(50),
-    ville VARCHAR2(50),
+    ville VARCHAR2(50)
 ) NOT FINAL;
 
 CREATE TYPE adresseWithEmail_type UNDER adresse_type (
-    adresseEmail VARCHAR2(50),
+    adresseEmail VARCHAR2(50)
 );
 
 CREATE TYPE personne_type AS OBJECT (
@@ -14,21 +14,21 @@ CREATE TYPE personne_type AS OBJECT (
     nom VARCHAR2(50),
     prenom VARCHAR2(50),
     adresse adresse_type,
-    age int,
+    age int
 ) NOT FINAL;
 
 CREATE TYPE etudiant_type UNDER personne_type (
     numCarteEtudiant VARCHAR2(50),
-    anneeInscription VARCHAR2(50),
+    anneeInscription VARCHAR2(50)
 )
 
 CREATE TYPE enseignant_type UNDER personne_type (
-    grade VARCHAR2(50),
+    grade VARCHAR2(50)
 )
 
 CREATE TABLE Personne OF personne_type (
     CONSTRAINT pk_personne PRIMARY KEY(numero),
-    CONSTRAINT c_age CHECK (age BETWEEN 17 AND 60),
+    CONSTRAINT c_age CHECK (age BETWEEN 17 AND 60)
 )
 
 INSERT INTO Personne VALUES (personne_type(100, 'KADI', 'Sonia', adresse_type(5, 'BENBOUALI Hassiba', 'Béjaia', '06000'), 30));
@@ -38,6 +38,12 @@ INSERT INTO Personne VALUES (etudiant_type('MI-2017-100', 'SALMA', 'Nabil', adre
 SELECT * FROM Personne;
 SELECT REF(p) FROM Personne p;
 SELECT p.numero, p.nom, p.prenom, TREAT(p.adresse AS adresseWithEmail_type).adresseEmail FROM Personne p;
-SELECT p.numero, p.nom, p.prenom, TREAT(VALUE(p) AS enseignant_type).grade FROM Personne p WHERE VALUE(p) OF (enseignant_type);
-SELECT p.numero, p.nom, p.prenom, TREAT(VALUE(p) AS etudiant_type).numCarteEtudiant FROM Personne p WHERE VALUE(p)OF (etudiant_type);
-SELECT p.* FROM Personne p WHERE VALUE(p) IS NOT OF (enseignant_type) OR VALUE(p) IS NOT OF(etudiant_type);
+SELECT p.numero, p.nom, p.prenom, TREAT(VALUE(p) AS enseignant_type).grade FROM Personne p WHERE VALUE(p) IS OF (enseignant_type);
+SELECT p.numero, p.nom, p.prenom, TREAT(VALUE(p) AS etudiant_type).numCarteEtudiant FROM Personne p WHERE VALUE(p) IS OF (etudiant_type);
+SELECT p.* FROM Personne p WHERE VALUE(p) IS NOT OF (enseignant_type) AND VALUE(p) IS NOT OF(etudiant_type);
+
+
+-- Bonus
+
+SELECT user FROM dual; -- Utilisateur actuel
+SELECT sys_context('userenv','instance_name') FROM dual; -- get SID (ORCLCDB / ORCLPDB)
---
+++ TP2 - BDDA.sql
@@ -0,0 +1,56 @@
+CREATE TYPE salle_type AS OBJECT (
+    numero VARCHAR2(20),
+    videopr CHAR(1)
+) NOT FINAL;
+
+CREATE TYPE salle_cours_type UNDER salle_type (
+    capacite NUMBER(3),
+    retropro CHAR(1),
+    micro CHAR(1)
+);
+
+CREATE TYPE salle_info_type UNDER salle_type (
+    nb_ordi NUMBER(2),
+    OS VARCHAR2(20)
+);
+
+CREATE TYPE ensm_type AS OBJECT (
+    code VARCHAR2(20),
+    effectif NUMBER(3),
+    videopr CHAR(1)
+) NOT FINAL;
+
+CREATE TYPE cr_type UNDER ensm_type (
+    retropro CHAR(1)
+);
+
+CREATE TYPE td_type UNDER ensm_type (
+    machine CHAR(1)
+);
+
+CREATE TYPE pgEnseignant_type AS OBJECT (
+    ref_ensm ref ensm_type,
+    ref_salle ref salle_type,
+    jour VARCHAR2(10),
+    heure_debut NUMBER(4,1),
+    heure_fin NUMBER(4,1)
+);
+
+CREATE TABLE salle OF salle_type (
+    CONSTRAINT pk_numero PRIMARY KEY(numero)
+);
+
+CREATE TABLE ensm OF ensm_type (
+    CONSTRAINT pk_code PRIMARY KEY(code)
+);
+
+CREATE TABLE pg_ensm OF pgEnseignant_type (
+    CONSTRAINT nn_ref_salle CHECK (ref_salle IS NOT NULL),
+    CONSTRAINT fk_ref_salle ref_salle REFERENCES salle,
+    CONSTRAINT nn_ref_ensm CHECK (ref_ensm IS NOT NULL),
+    CONSTRAINT fk_ref_ensm ref_ensm REFERENCES ensm
+);
+
+INSERT INTO ensm VALUES(cr_type('BDDA', 250, 'O', 'N'));
+
+INSERT INTO salle VALUES(salle_cours_type('B8S22', 'N', 300, 'O', 'N'));
